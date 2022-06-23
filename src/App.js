e962e75011431c300be9738b9a07ee702f7d5dbb
vitugo <vitorpacheco_14@hotmail.com>
2022-06-23 17:03:42 +0100

    feat: Drag to Scroll

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Categories from './features/categories/Categories';
 import Products from './features/products/Products';
 import ShoppingCart from './features/shopping-cart/ShoppingCart';
 import HttpClient from './gateways/http.client';
+import dragToScroll from './utils/dragToScroll.utils';
 
 function App() {
   HttpClient.initialize();
 
+  useEffect(() => {
+    dragToScroll({ elementId: "categories" });
+    dragToScroll({ elementId: "products" });
+  }, []);
+
   return (
     <div className="grid grid-cols-11 gap4 h-screen w-screen">
-      <div className="col-span-2 w-full h-full overflow-auto">
+      <div id="categories" className="col-span-2 w-full h-full overflow-auto">
         <Categories />
       </div>
-      <div className="col-span-6 w-full h-full bg-gray-300 overflow-auto">
+      <div id="products" className="col-span-6 w-full h-full bg-gray-300 overflow-auto">
         <Products />
       </div>
       <div className="col-span-3 w-full h-full overflow-auto">
--- src/features/shopping-cart/ShoppingCart.jsx
+++ src/features/shopping-cart/ShoppingCart.jsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getItemsFromCart,
-  removeItemFromCart,
-} from "./shoppingCartSlice";
+import { getItemsFromCart, removeItemFromCart } from "./shoppingCartSlice";
 import { printEuro } from "../../utils/currency.utils";
 
 import ShoppingCartHeader from "./ShoppingCartHeader";
 import ShoppingCartFooter from "./ShoppingCartFooter";
+import dragToScroll from "../../utils/dragToScroll.utils";
 
 const ShoppingCart = () => {
   const dispatch = useDispatch();
   const cartItems = useSelector(getItemsFromCart);
+
+  useEffect(() => {
+    dragToScroll({ elementId: "shopping-cart-list" });
+  }, []);
 
   if (!cartItems) {
     return <></>;
@@ -22,12 +24,16 @@ const ShoppingCart = () => {
       <div className="flex flex-col p-4 h-screen w-full">
         <ShoppingCartHeader />
 
-        <div className="mb-auto h-full w-full overflow-auto">
+        <div
+          id="shopping-cart-list"
+          className="mb-auto h-full w-full overflow-auto cursor-grab"
+        >
           {Object.entries(cartItems).map(([itemId, item], index) => (
             <div
               className={`text-2xl ${
                 index % 2 === 0 && "bg-gray-200"
               } p-2 rounded-md`}
+              key={itemId}
             >
               <div className="grid grid-cols-2" key={itemId}>
                 <div className="col-auto truncate">
